6ecd7b2d348cf168a7e1fe2711881abcaacc3bd1
Yang Sheng Han <progamesigner@gmail.com>
2020-02-10 23:31:54 +0800

    Add lints and format `webpack.config.js`

--- webpack.config.js
+++ webpack.config.js
@@ -2,57 +2,57 @@ const path = require('path')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
-    entry: './src/scripts/main.js',
-    output: {
-        path: path.resolve(__dirname, 'layouts/partials'),
-        filename: '[name].min.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.scss$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            importLoaders: 1
-                        }
-                    },
-                    {
-                        loader: 'postcss-loader',
-                        options: {
-                            plugins: [
-                                require('@fullhuman/postcss-purgecss')({
-                                    content: [
-                                        './**/*.html'
-                                    ],
-                                    keyframes: true,
-                                    fontFace: true
-                                }),
-                                require('cssnano')({
-                                    preset: [
-                                        'default',
-                                        {
-                                            autoprefixer: {
-                                            },
-                                            discardComments: {
-                                                removeAll: true
-                                            }
-                                        }
-                                    ]
-                                })
-                            ]
-                        }
-                    },
-                    'sass-loader'
-                ]
+  entry: './src/scripts/main.js',
+  output: {
+    path: path.resolve(__dirname, 'layouts/partials'),
+    filename: '[name].min.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1
             }
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: [
+                require('@fullhuman/postcss-purgecss')({
+                  content: [
+                    './**/*.html'
+                  ],
+                  fontFace: true,
+                  keyframes: true
+                }),
+                require('cssnano')({
+                  preset: [
+                    'default',
+                    {
+                      autoprefixer: {
+                      },
+                      discardComments: {
+                        removeAll: true
+                      }
+                    }
+                  ]
+                })
+              ]
+            }
+          },
+          'sass-loader'
         ]
-    },
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: '[name].min.css'
-        })
+      }
     ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].min.css'
+    })
+  ]
 }
